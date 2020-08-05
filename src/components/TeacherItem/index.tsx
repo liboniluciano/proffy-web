9a639d3c69bf7d28ea05600c5be712eb6ed74d92
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-05 17:41:50 -0300

    feat: fetch teachers with filters

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import'./styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+    id: number
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string  
+} ;
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const target = "_blank";
+  function createNewConnection(){
+    api.post('/connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/26471185?s=460&u=bacc06ad3d9c692f64a7cbf9a11bfe9fa79c831f&v=4" alt="Luciano Liboni"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Luciano Liboni</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt 
-        <br/> <br/>
-        repudiandae, accusantium architecto, voluptate corporis obcaecati 
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button" >
+        <a 
+            target={target}
+            onClick={createNewConnection} 
+            href={`https://wa.me/${teacher.whatsapp}`} >
           <img src={whatsappIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import Input from '../../components/Input';
+import React, { FormEvent, useState } from 'react';
 
+import api from '../../services/api';
+
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import './styles.css';
 
+
+
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState([]);
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  async function searchTeachers(e: FormEvent){
+    e.preventDefault();
+
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    });
+
+    setTeachers(response.data);
+
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers}>
         <Select 
           name="subject" 
-          label="Matéria" 
+          label="Matéria"
+          value={subject}
+          onChange={(e) => {setSubject(e.target.value)}} 
           options={[
             { value: 'Artes', label: 'Artes'},
             { value: 'Biologia', label: 'Biologia'},
@@ -29,7 +55,9 @@ const TeacherList: React.FC = () => {
         />
         <Select 
           name="week_day" 
-          label="Dia da semana" 
+          label="Dia da semana"
+          value={week_day}
+          onChange={(e) => {setWeekDay(e.target.value)}}  
           options={[
             { value: '0', label: 'Domingo'},
             { value: '1', label: 'Segunda-feira'},
@@ -40,14 +68,22 @@ const TeacherList: React.FC = () => {
             { value: '6', label: 'Sábado'},
             ]}
           />
-          <Input name="time" type="time" label="Hora"/>
+          <Input 
+            name="time" 
+            type="time" 
+            label="Hora"
+            value={time}
+            onChange={(e) => {setTime(e.target.value)}} 
+          />
+
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+        return <TeacherItem key={teacher.id}  teacher={teacher}/>;
+        })}
       </main>
     </div>
   );
